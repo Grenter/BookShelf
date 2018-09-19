930a41d04c8bfc3d2e3bac3b6cf009917369ec15
David Grenter <davidgrenter@gmail.com>
2018-09-19 16:05:46 +0100

    Add AddBook ui

---
+++ BookShelf.UI/Model/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShelf.UI.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string CoverImage { get; set; }
+        public string Shelf { get; set; }
+        public string Genre { get; set; }
+        public bool Own { get; set; }
+    }
+}
