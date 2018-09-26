ec2f42242688c788cf939cd4de9d1dbd81771c03
David Grenter <davidgrenter@gmail.com>
2018-09-26 15:26:20 +0100

    Fixed AddBook to closure on save

---
+++ BookShelf.UI/Pages/BookViewModel.cs
@@ -0,0 +1,47 @@
+﻿using BookShelf.Shared.Model;
+using BookShelf.UI.Services;
+using Microsoft.AspNetCore.Blazor.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookShelf.UI.Pages
+{
+    public class BookViewModel : BlazorComponent
+    {
+        [Inject]
+        private BookService BookService { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public bool DisplayAdd;
+
+        protected override async Task OnInitAsync()
+        {
+            await GetBooks();
+        }
+
+        public async Task Reset()
+        {
+            Books = null;
+            var success = await BookService.Reset();
+
+            if (success)
+            {
+                await GetBooks();
+            }
+        }
+
+        public async Task GetBooks()
+        {
+            Books = await BookService.GetBooks();
+        }
+
+        public async Task Save(Book book)
+        {
+            await BookService.PostBook(book);
+            DisplayAdd = !DisplayAdd;
+            await GetBooks();
+            StateHasChanged();
+        }
+    }
+}
