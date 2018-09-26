888c8ec13f20f20daccdd593c6797c43ec15c0ca
David Grenter <davidgrenter@gmail.com>
2018-09-26 10:38:23 +0100

    Add ResetBooks function to return dynamodb to a sensible state for github pages demo purposes

--- BookShelf.Lambda/BookFunction.cs
+++ BookShelf.Lambda/BookFunction.cs
@@ -14,6 +14,12 @@ namespace BookShelf.Lambda
 {
     public class BookFunction
     {
+        private readonly IList<string> _keepList = new List<string>
+        {
+            "80fb277a-049f-44f6-9f5a-7757dd8388d9",
+            "e13f4d30-6318-4c9d-b49c-f92edc473fcf"
+        };
+
         public APIGatewayProxyResponse GetBooks(APIGatewayProxyRequest request, ILambdaContext context)
         {
             var dbContext = DynamoDbUtil.BuildContext();
@@ -23,6 +29,25 @@ namespace BookShelf.Lambda
             var books = booksSerch.Result;
 
             return ResponseBuilder.Http200(JsonConvert.SerializeObject(books), new Dictionary<string, string> { { "Content-Type", "application/json" } });
+        }
+
+        public APIGatewayProxyResponse ResetBooks(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var dbContext = DynamoDbUtil.BuildContext();
+
+            var booksSerch = dbContext.ScanAsync<Book>(new List<ScanCondition>()).GetRemainingAsync();
+
+            var books = booksSerch.Result;
+
+            foreach (var book in books)
+            {
+                if (!_keepList.Contains(book.Id))
+                {
+                    dbContext.DeleteAsync(book).Wait();
+                }
+            }
+
+            return ResponseBuilder.Http200(JsonConvert.SerializeObject("Success"), new Dictionary<string, string> {{"Content-Type", "application/json"}});
         }
 
         public APIGatewayProxyResponse PostBook(APIGatewayProxyRequest request, ILambdaContext context)
