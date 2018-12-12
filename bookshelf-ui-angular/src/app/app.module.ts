1e1f88f2cf69b5134e20a37b09a783dbaff14784
David Grenter <davidgrenter@gmail.com>
2018-12-12 13:46:33 +0000

    Add add-book component

--- bookshelf-ui-angular/src/app/app.module.ts
+++ bookshelf-ui-angular/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { ControlsComponent } from './controls/controls.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     BookListComponent,
-    ControlsComponent
+    ControlsComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
