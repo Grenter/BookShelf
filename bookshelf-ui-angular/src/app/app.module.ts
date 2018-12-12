bd720243a8252e874b57db83ae7e95c58cc038df
David Grenter <davidgrenter@gmail.com>
2018-12-12 11:22:30 +0000

    Add controls component

--- bookshelf-ui-angular/src/app/app.module.ts
+++ bookshelf-ui-angular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    BookListComponent
+    BookListComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
